5da0f9c97eb46243aff1a7f9f326a76eea70d4a7
Teresa Kariuki <108309963+kariukit@users.noreply.github.com>
2022-08-24 22:32:57 -0400

    starting new js code
    declaring variables and elements as well as creating a function for the questions and for the timer

---
+++ Quizz App/code-quiz.js
@@ -0,0 +1,75 @@
+const welcomeEl = document.querySelector("#welcome");
+const startQuizBtnEl = document.querySelector("#startQuiz");
+
+
+const quizEl = document.querySelector("#quiz");
+const questionEl = document.querySelector("#question");
+const answersEl = document.querySelector("#answers");
+
+
+const inputScoreEl = document.querySelector("#inputScore");
+const initialsEl = document.querySelector("#initials");
+const submitInitialsBtnEl = document.querySelector("#submitInitials");
+const userScoreEl = document.querySelector("#score");
+
+
+const highScoresEl = document.querySelector("#highScores");
+const scoresEl = document.querySelector("#scores");
+const goBackBtnEl = document.querySelector("#goBack");
+const clearScoresBtnEl = document.querySelector("#clearScores");
+
+
+const viewHScoresBtnEl = document.querySelector("#viewHScores");
+const timerEl = document.querySelector("#timer");
+var score = 0;
+var currentQ = 0;
+var highScores = [];
+var interval;
+var timeGiven = 75;
+var secondsElapsed = 0;
+
+
+function startTimer() {
+    timerEl.textContent = timeGiven;
+    interval = setInterval(function () {
+        secondsElapsed++;
+        timerEl.textContent = timeGiven - secondsElapsed;
+        if (secondsElapsed >= timeGiven) {
+            currentQ = questions.length;
+            nextQuestion();
+        }
+    }, 1000);
+}
+
+
+function stopTimer() {
+    clearInterval(interval);
+}
+
+
+function nextQuestion() {
+    currentQ++;
+    if (currentQ < questions.length) {
+        renderQuestion();
+    } else {
+        stopTimer();
+        if ((timeGiven - secondsElapsed) > 0)
+            score += (timeGiven - secondsElapsed);
+        userScoreEl.textContent = score;
+        hide(quizEl);
+        show(inputScoreEl);
+        timerEl.textContent = 0;
+    }
+}
+
+
+function checkAnswer(answer) {
+    if (questions[currentQ].answer == questions[currentQ].choices[answer.id]) {
+        score += 5;
+        displayMessage("Correct!");
+    }
+    else {
+        secondsElapsed += 10;
+        displayMessage("Wrong...");
+    }
+}
